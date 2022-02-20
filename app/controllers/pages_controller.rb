18f5d47986e9115bf7f2156140bc7c6f5b548303
Jordano Moscoso <cesar_notes@outlook.com>
2022-02-20 10:46:47 -0500

    add custom example

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def index
-    render text: 'Hello', content_type: 'text/plain'
+    render text: 'Hello', content_type: 'text/plain', status: 200
   end
 end
